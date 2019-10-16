b33a3890160a9562f69e3c9d656b8720392d0564
Franck SIMON <franck.simon@antislashn.org>
2019-10-16 17:11:52 +0200

    Etape 1 : ajout du listener

---
+++ src/main/java/org/antislashn/communes/services/CommuneServices.java
@@ -0,0 +1,20 @@
+package org.antislashn.communes.services;
+
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.antislashn.communes.dao.CommuneDAO;
+import org.antislashn.communes.entities.Commune;
+
+public class CommuneServices {
+	private CommuneDAO dao;
+	
+	public CommuneServices(EntityManagerFactory emf) {
+		dao = new CommuneDAO(emf);
+	}
+	
+	public List<Commune> getCommunesByCodePostal(String codePostal){
+		return dao.getCommunesByCodePostal(codePostal);
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,5 +8,8 @@
 </head>
 <body>
 <h2>Recherche par code postal</h2>
+<form action="CommuneServlet">
+Code postal <input name="cp" id="cp" size="8"> <button>Rechercher</button>
+</form>
 </body>
 </html>
